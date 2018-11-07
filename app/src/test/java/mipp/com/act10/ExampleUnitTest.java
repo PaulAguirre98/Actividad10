5d2932077ee4dcfc269c3af66dec8a5f120c9be3
Alfredo Paul Aguirre Torres <alfpaul98@hotmail.com>
2018-11-06 23:20:20 -0600

    Initial commit

---
+++ app/src/test/java/mipp/com/act10/ExampleUnitTest.java
@@ -0,0 +1,12 @@
+package mipp.com.act10;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
